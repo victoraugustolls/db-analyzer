93b81b5677f10787057d8d85bb3d7131ad76eca8
victorsouza <victoraugustolls@users.noreply.github.com>
2022-06-02 22:36:16 -0300

    feat: simplified analyzer

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -34,25 +34,6 @@ class Analyzer:
 
         return node
 
-        # # Order the leaves in descending order by delta.
-        # # This means that leaves with higher cost reductions comes first on the list.
-        # # self._leaves.sort(key=lambda x: (int(x.gain), -x.cost), reverse=True)
-        # self._leaves.sort(key=lambda x: int(x.delta), reverse=True)
-        #
-        # result = Result()
-        # for leaf in self._leaves:
-        #     tree: list[Node] = []
-        #     current = leaf
-        #     while current.parent is not None:
-        #         tree.append(current)
-        #         current = current.parent
-        #
-        #     # Reverse the tree to show the actions in the order they should be applied.
-        #     tree.reverse()
-        #     result.nodes.append(tree)
-        #
-        # return result
-
     async def _mount(self, node: Node, actions: list[Command]) -> Node:
         cumulative_gain = 0
 
@@ -89,17 +70,7 @@ class Analyzer:
 
             node.add_child(child=new_node)
 
-            # If the cost was higher than the gains, consider the current node as a leaf:
-            if cost > gain:
-                self._leaves.append(node)
-
             # Rollback the executed action.
             await action.rollback()
-
-        # If there were no gains on this node, there can be two causes:
-        #   1. There were no more actions to perform -> it's a leaf
-        #   2. There were no actions that resulted in a cost reduction -> it's a leaf
-        if cumulative_gain == 0:
-            self._leaves.append(node)
 
         return node
